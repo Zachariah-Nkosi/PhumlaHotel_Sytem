8b5f3c7e530f5d3893bc0be31ef96abc39891d2e
unknown <Student@LIB-LWTR-001.wf.uct.ac.za>
2024-09-21 17:42:44 +0200

    DB class, refress dataset and update data source methods

---
+++ database/DB.cs
@@ -0,0 +1,90 @@
+﻿using phumla_kamnandi_83.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phumla_kamnandi_83.database
+{
+    public class DB
+    {
+        #region Data Fields
+        private string strConn = Settings.Default.PhumlaConnString;
+        private DataSet dsMain;
+        private SqlConnection sqlConn;
+        private SqlDataAdapter dataAdapter;
+        #endregion
+
+        #region Constructor
+        public DB ()
+        {
+            try
+            {
+                
+                sqlConn = new SqlConnection(strConn);
+                sqlConn.Open();
+
+                dsMain = new DataSet();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }finally
+            {
+                sqlConn.Close();
+            }
+        }
+        #endregion
+
+        #region Update Data Set
+        public void FillDataSet(string sqlStatement, string aTable)
+        {
+            try
+            {
+                dataAdapter = new SqlDataAdapter(sqlStatement, sqlConn);
+                sqlConn.Open();
+                dsMain.Clear();
+
+                dataAdapter.Fill(dsMain, aTable);
+
+                sqlConn.Close ();
+
+            }catch (Exception e)
+            {
+                MessageBox.Show (e.Message + " " + e.StackTrace);
+            }
+        }
+        #endregion
+
+        #region UpdateDataSource
+        public bool UpdateDataSource(string sqlLocal, string table)
+        {
+            bool success;
+            try
+            {
+                sqlConn.Open();
+
+                dataAdapter.Update(dsMain, table);  //update database via data adapter
+
+                FillDataSet(sqlLocal, table);       //refresh the dataset
+                success = true;
+            }
+            catch (Exception errObj)
+            {
+                MessageBox.Show(errObj.Message + " " + errObj.StackTrace);
+                success = false;
+            }
+            finally
+            {
+                sqlConn.Close();
+            }
+            return success;
+        }
+        #endregion
+    }
+}
